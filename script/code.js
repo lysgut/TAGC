168acc5c90d09c8f06fe181273f3d15d46d56589
lysgut <ltuninetticivarolo@escuelasproa.edu.ar>
2023-08-15 12:09:52 -0300

    comiytygyy

--- script/code.js
+++ script/code.js
@@ -6,10 +6,10 @@ function random(x){
     return numeroFinal
 }
 
-function cadenaADN(){
+function cadenaADN(x){
     let cadena = 'ATG'
     let fin = ['TAA','TAG', 'TGA']
-    for(let i = 0; i < 12; i++){
+    for(let i = 0; i < x-6; i++){
         cadena += CATG[random(4)]
     }
     cadena+= fin[random(3)]
@@ -39,11 +39,30 @@ function cadenaADN(){
         }
     }
     let adn = {
-        'helice1' : cadena,
-        'helice2' : cadena2,
-        'arn' : arn,
-    }
-    return adn
+    helice1 : cadena, 
+    helice2 : cadena2, 
+    ARN : arn 
 }
 
-console.log(cadenaADN())
+
+
+    return adn
+    
+}
+
+
+
+const boton = document.getElementById('h');
+const C1HTML= document.getElementById("c1");
+const C2HTML = document.getElementById("c2");
+const ARNHTML = document.getElementById("arn")
+
+boton.addEventListener("click", function() {
+    console.log("generando")
+    let newADN = cadenaADN(18)
+    C1HTML.textContent = newADN.helice1
+    C2HTML.textContent = newADN.helice2
+    ARNHTML.textContent = newADN.ARN
+
+});
+console.log(cadenaADN(18))
